8f562d4fdd09aa19e133d2217a8604433324c1da
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-01-07 11:27:36 +0100

    Create CorsConfig.java

---
+++ src/main/java/com/elitetech/springsecurity/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.elitetech.springsecurity.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Vous pouvez restreindre les URL ici
+                        .allowedOrigins("http://localhost:4200") // URL de votre application Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
